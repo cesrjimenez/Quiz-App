a533e3643fc181fdb377b3b40138ba50f53a62af
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2014-06-16 18:26:00 -0700

    First pages commit

---
+++ js/app.js
@@ -0,0 +1,395 @@
+var Ext = 0;
+var Int = 0;
+var Sen = 0;
+var Intu = 0;
+var Thin = 0;
+var Feel = 0;
+var Judg = 0;
+var Perc = 0;
+
+$(document).ready(function(){
+    $(".a1-left").click(function(){
+        Ext++;
+        console.log("Extroverted = " + Ext);
+        $(".a1").hide();
+        $(".b1").fadeIn("slow");
+    });
+
+    $(".b1-left").click(function(){
+        Ext++;
+        console.log("Extroverted = " + Ext);
+        $(".b1").hide();
+        $(".c1").fadeIn("slow");
+    });
+
+    $(".c1-left").click(function(){
+        Ext++;
+        console.log("Extroverted = " + Ext);
+        $(".c1").hide();
+        $(".d1").fadeIn("slow");
+    });
+
+    $(".d1-left").click(function(){
+        Ext++;
+        console.log("Extroverted = " + Ext);
+        $(".d1").hide();
+        $(".e1").fadeIn("slow");
+    });
+
+    $(".e1-left").click(function(){
+        Ext++;
+        console.log("Extroverted = " + Ext);
+        $(".e1").hide();
+        $(".a2").fadeIn("slow");
+    });
+
+    $(".a2-left").click(function(){
+        Sen++;
+        console.log("Sensing = " + Sen);
+        $(".a2").hide();
+        $(".b2").fadeIn("slow");
+    });
+
+    $(".b2-left").click(function(){
+        Sen++;
+        console.log("Sensing = " + Sen);
+        $(".b2").hide();
+        $(".c2").fadeIn("slow");
+    });
+
+    $(".c2-left").click(function(){
+        Sen++;
+        console.log("Sensing = " + Sen);
+        $(".c2").hide();
+        $(".d2").fadeIn("slow");
+    });
+
+    $(".d2-left").click(function(){
+        Sen++;
+        console.log("Sensing = " + Sen);
+        $(".d2").hide();
+        $(".e2").fadeIn("slow");
+    });
+
+    $(".e2-left").click(function(){
+        Sen++;
+        console.log("Sensing = " + Sen);
+        $(".e2").hide();
+        $(".a3").fadeIn("slow");
+    });
+
+    $(".a3-left").click(function(){
+        Thin++;
+        console.log("Thinking = " + Thin);
+        $(".a3").hide();
+        $(".b3").fadeIn("slow");
+    });
+
+    $(".b3-left").click(function(){
+        Thin++;
+        console.log("Thinking = " + Thin);
+        $(".b3").hide();
+        $(".c3").fadeIn("slow");
+    });
+
+    $(".c3-left").click(function(){
+        Thin++;
+        console.log("Thinking = " + Thin);
+        $(".c3").hide();
+        $(".d3").fadeIn("slow");
+    });
+
+    $(".d3-left").click(function(){
+        Thin++;
+        console.log("Thinking = " + Thin);
+        $(".d3").hide();
+        $(".e3").fadeIn("slow");
+    });
+
+    $(".e3-left").click(function(){
+        Thin++;
+        console.log("Thinking = " + Thin);
+        $(".e3").hide();
+        $(".a4").fadeIn("slow");
+    });
+
+    $(".a4-left").click(function(){
+        Judg++;
+        console.log("Judging = " + Judg);
+        $(".a4").hide();
+        $(".b4").fadeIn("slow");
+    });
+
+    $(".b4-left").click(function(){
+        Judg++;
+        console.log("Judging = " + Judg);
+        $(".b4").hide();
+        $(".c4").fadeIn("slow");
+    });
+
+    $(".c4-left").click(function(){
+        Judg++;
+        console.log("Judging = " + Judg);
+        $(".c4").hide();
+        $(".d4").fadeIn("slow");
+    });
+
+    $(".d4-left").click(function(){
+        Judg++;
+        console.log("Judging = " + Judg);
+        $(".d4").hide();
+        $(".e4").fadeIn("slow");
+    });
+
+    $(".e4-left").click(function(){
+        Judg++;
+        console.log("Judging = " + Judg);
+        $(".e4").hide();
+        $(".resultBox").fadeIn("slow");
+    });
+
+/* Right button counter*/
+
+
+    $(".a1-right").click(function(){
+        Int++;
+        console.log("Introverted = " + Int);
+        $(".a1").hide();
+        $(".b1").fadeIn("slow");
+    });
+
+    $(".b1-right").click(function(){
+        Int++;
+        console.log("Introverted = " + Int);
+        $(".b1").hide();
+        $(".c1").fadeIn("slow");
+    });
+
+    $(".c1-right").click(function(){
+        Int++;
+        console.log("Introverted = " + Int);
+        $(".c1").hide();
+        $(".d1").fadeIn("slow");
+    });
+
+    $(".d1-right").click(function(){
+        Int++;
+        console.log("Introverted = " + Int);
+        $(".d1").hide();
+        $(".e1").fadeIn("slow");
+    });
+
+    $(".e1-right").click(function(){
+        Int++;
+        console.log("Introverted = " + Int);
+        $(".e1").hide();
+        $(".a2").fadeIn("slow");
+    });
+
+    $(".a2-right").click(function(){
+        Intu++;
+        console.log("Intuition = " + Intu);
+        $(".a2").hide();
+        $(".b2").fadeIn("slow");
+    });
+
+    $(".b2-right").click(function(){
+        Intu++;
+        console.log("Intuition = " + Intu);
+        $(".b2").hide();
+        $(".c2").fadeIn("slow");
+    });
+
+    $(".c2-right").click(function(){
+        Intu++;
+        console.log("Intuition = " + Intu);
+        $(".c2").hide();
+        $(".d2").fadeIn("slow");
+    });
+
+    $(".d2-right").click(function(){
+        Intu++;
+        console.log("Intuition = " + Intu);
+        $(".d2").hide();
+        $(".e2").fadeIn("slow");
+    });
+
+    $(".e2-right").click(function(){
+        Intu++;
+        console.log("Intuition = " + Intu);
+        $(".e2").hide();
+        $(".a3").fadeIn("slow");
+    });
+
+    $(".a3-right").click(function(){
+        Feel++;
+        console.log("Feeling = " + Feel);
+        $(".a3").hide();
+        $(".b3").fadeIn("slow");
+    });
+
+    $(".b3-right").click(function(){
+        Feel++;
+        console.log("Feeling = " + Feel);
+        $(".b3").hide();
+        $(".c3").fadeIn("slow");
+    });
+
+    $(".c3-right").click(function(){
+        Feel++;
+        console.log("Feeling = " + Feel);
+        $(".c3").hide();
+        $(".d3").fadeIn("slow");
+    });
+
+    $(".d3-right").click(function(){
+        Feel++;
+        console.log("Feeling = " + Feel);
+        $(".d3").hide();
+        $(".e3").fadeIn("slow");
+    });
+
+    $(".e3-right").click(function(){
+        Feel++;
+        console.log("Feeling = " + Feel);
+        $(".e3").hide();
+        $(".a4").fadeIn("slow");
+    });
+
+    $(".a4-right").click(function(){
+        Perc++;
+        console.log("Perceiving = " + Perc);
+        $(".a4").hide();
+        $(".b4").fadeIn("slow");
+    });
+
+    $(".b4-right").click(function(){
+        Perc++;
+        console.log("Perceiving = " + Perc);
+        $(".b4").hide();
+        $(".c4").fadeIn("slow");
+    });
+
+    $(".c4-right").click(function(){
+        Perc++;
+        console.log("Perceiving = " + Perc);
+        $(".c4").hide();
+        $(".d4").fadeIn("slow");
+    });
+
+    $(".d4-right").click(function(){
+        Perc++;
+        console.log("Perceiving = " + Perc);
+        $(".d4").hide();
+        $(".e4").fadeIn("slow");
+    });
+
+    $(".e4-right").click(function(){
+        Perc++;
+        console.log("Perceiving = " + Perc);
+        $(".e4").hide();
+        $(".resultBox").fadeIn("slow");
+    });
+
+});
+
+$(".what").click(function(){
+    $(".overlay").fadeIn(1400);
+
+});
+
+$(".Button-1").click(function(){
+    $(".a1").fadeIn("slow");
+    $(".Button-1").hide();
+});
+
+$(".Button-2").click(function(){
+    $(".overlay").fadeOut(1400);
+});
+
+
+$(".resultBox").click(function(){
+    result();
+    $(".resultBox").hide();
+    $(".overlayResults").fadeIn(1400);
+});
+
+$(".newGame").click(function(){
+    $(".overlay").fadeOut(1400);
+    $(".overlayResults").fadeOut(1400);
+    $(".Button-1").fadeIn("slow");
+    $(".a1").hide();
+    $(".b1").hide();
+    $(".c1").hide();
+    $(".d1").hide();
+    $(".e1").hide();
+    $(".a2").hide();
+    $(".b2").hide();
+    $(".c2").hide();
+    $(".d2").hide();
+    $(".e2").hide();
+    $(".a3").hide();
+    $(".b3").hide();
+    $(".c3").hide();
+    $(".d3").hide();
+    $(".e3").hide();
+    $(".a4").hide();
+    $(".b4").hide();
+    $(".c4").hide();
+    $(".d4").hide();
+    $(".e4").hide();
+    var Ext = 0;
+    var Int = 0;
+    var Sen = 0;
+    var Intu = 0;
+    var Thin = 0;
+    var Feel = 0;
+    var Judg = 0;
+    var Perc = 0;
+    console.log(Ext);
+    console.log(Int);
+    console.log(Sen);
+    console.log(Intu);
+    console.log(Thin);
+    console.log(Feel);
+    console.log(Judg);
+    console.log(Perc);
+});
+
+function result() {
+    if(Int >= 3 && Sen >= 3 && Thin >= 3 && Judg >= 3) {
+        $(".ISTJ").fadeIn("slow");
+    } else if(Int >= 3 && Sen >= 3 && Thin >= 3 && Perc >= 3){
+        $(".ISTP").fadeIn("slow");
+    } else if(Ext >= 3 && Sen >= 3 && Thin >= 3 && Perc >= 3) {
+        $(".ESTP").fadeIn("slow");
+    } else if(Ext >= 3 && Sen >= 3 && Thin >= 3 && Judg >= 3) {
+        $(".ESTJ").fadeIn("slow");
+    } else if(Int >= 3 && Sen >= 3 && Feel >= 3 && Judg >= 3){
+        $(".ISFJ").fadeIn("slow");
+    } else if(Int >= 3 && Sen >= 3 && Feel >= 3 && Perc >= 3){
+        $(".ISFP").fadeIn("slow");
+    } else if(Ext >= 3 && Sen >= 3 && Feel >= 3 && Perc >= 3){
+        $(".ESFP").fadeIn("slow");
+    } else if(Ext >= 3 && Sen >= 3 && Feel >= 3 && Judg >= 3){
+        $(".ESFJ").fadeIn("slow");
+    } else if(Int >= 3 && Intu >= 3 && Feel >= 3 && Judg >= 3){
+        $(".INFJ").fadeIn("slow");
+    } else if(Int >= 3 && Intu >= 3 && Feel >= 3 && Perc >= 3){
+        $(".INFP").fadeIn("slow");
+    } else if(Ext >= 3 && Intu >= 3 && Feel >= 3 && Perc >= 3){
+        $(".ENFP").fadeIn("slow");
+    } else if(Ext >= 3 && Intu >= 3 && Feel >= 3 && Judg >= 3) {
+        $(".ENFJ").fadeIn("slow");
+    } else if(Int >= 3 && Intu >= 3 && Thin >= 3 && Judg >= 3){
+        $(".INTJ").fadeIn("slow");
+    } else if(Int >= 3 && Intu >= 3 && Thin >= 3 && Perc >= 3){
+        $(".INTP").fadeIn("slow");
+    } else if(Ext >= 3 && Intu >= 3 && Thin >= 3 && Perc >= 3){
+        $(".ENTP").fadeIn("slow");
+    } else if(Ext >= 3 && Intu >= 3 && Thin >= 3 && Judg >= 3) {
+        $(".ENTJ").fadeIn("slow");
+    } else {
+        console.log("Doesn't work!");
+    }
+}
